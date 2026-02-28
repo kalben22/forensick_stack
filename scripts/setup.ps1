5a52b11db1af85cdb2d3b9f2bce1b8de95c89b01
kalben22 <bernardhoungbo2021@gmail.com>
2026-02-28 15:05:19 +0100

    ForensicStack Frontend setup

--- scripts/setup.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Building ForensicStack..."
-docker compose build
-
-Write-Host "Starting services..."
-docker compose up -d
-
-Write-Host "ForensicStack is running at:"
-Write-Host "http://localhost:8000"
